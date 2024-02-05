002177c528c1f73b363ba24f31aababea96744dc
ljsnell <ljsnell7@gmail.com>
2024-02-04 20:34:31 -0600

    Updated to add blood emitting

--- main.lua
+++ main.lua
@@ -35,15 +35,18 @@ mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.SongOfSolomonUse, sosBook)
 local exodusBook = Isaac.GetItemIdByName("Exodus")
 function mod:ExodusUse(item)
     local door_closed = true
+    local room = Game():GetRoom()
 
     while door_closed do
-        local grid_entity_door = Game():GetRoom():GetDoor(math.random(0,7))
+        local grid_entity_door = room:GetDoor(math.random(0,7))
         if grid_entity_door ~= nil then
             grid_entity_door:Open()
             door_closed = false
         end
     end
-    
+
+    room:EmitBloodFromWalls(5, 25)
+
     return {
         Discharge = true,
         Remove = false,
