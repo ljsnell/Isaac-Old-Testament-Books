ae50652d2aa321730fff4b97aba222f894b0af7f
ljsnell <ljsnell@users.noreply.github.com>
2024-02-04 20:44:14 -0600

    Exodus (#2)
    * Something's broken.
    
    * It was working I just didn't know how to use the terminal correctly.
    
    * Probably not the most efficient way to do it but it works!
    
    * Updated to add blood emitting

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Mod for the Binding of Isaac based on books of the old testament.
 
 # Spawn command
-spawn 5.100.733
+Song of Solomon: spawn 5.100.733
+Exodus: spawn 5.100.734
--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 -- Item Ideas --
--- Song of Solomon: Charms all enemies in room, 4 room charge ✅
--- Exodus: Opens a random door in the room (Like doorstop) ☑
 -- Numbers: Increase one random stat by +.7. 6 room charge ☑
 -- Lamentations: Doubles creep damage ☑
 -- Judges: Spawns a random beggar, 6 room charge
@@ -9,10 +7,12 @@
 -- Jonah: Resets angel chance to 100% on pickup, +1 speed, +.5 damage
 ----------------
 
+local game = Game()
 local mod = RegisterMod("Old Testament", 1)
 
-local sosBook = Isaac.GetItemIdByName("Song of Solomon")
 
+-- Song of Solomon: Charms all enemies in room, 4 room charge ✅
+local sosBook = Isaac.GetItemIdByName("Song of Solomon")
 function mod:SongOfSolomonUse(item)    
     local roomEntities = Isaac.GetRoomEntities()
     for _, entity in ipairs(roomEntities) do
@@ -29,3 +29,29 @@ function mod:SongOfSolomonUse(item)
 end
 
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.SongOfSolomonUse, sosBook)
+
+
+-- Exodus: Opens a random door in the room (Like doorstop) Maybe use -isFirstEnemyDead?
+local exodusBook = Isaac.GetItemIdByName("Exodus")
+function mod:ExodusUse(item)
+    local door_closed = true
+    local room = Game():GetRoom()
+
+    while door_closed do
+        local grid_entity_door = room:GetDoor(math.random(0,7))
+        if grid_entity_door ~= nil then
+            grid_entity_door:Open()
+            door_closed = false
+        end
+    end
+
+    room:EmitBloodFromWalls(5, 25)
+
+    return {
+        Discharge = true,
+        Remove = false,
+        ShowAnim = true
+    }
+end
+
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.ExodusUse, exodusBook)
