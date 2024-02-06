0b1b9c9bbba63a135792cb39b3600f5d78cf92f5
ljsnell <ljsnell@users.noreply.github.com>
2024-02-05 22:45:15 -0600

    Jonah neutral (#3)
    * Untested so I'm going to assume it works. + ideas from Kevin.
    
    * Added the icons.
    
    * Updated readme
    
    * Need to update the exclamation_mark.png a bit but it works!
    
    * Would be nice to replace with actual fountain animation.
    
    * Renamed for clarity.

--- main.lua
+++ main.lua
@@ -1,17 +1,23 @@
 -- Item Ideas --
 -- Numbers: Increase one random stat by +.7. 6 room charge ☑
+    -- Numbers: Guy who runs someone through with a spear (double damage for subsequent piercing shots)
 -- Lamentations: Doubles creep damage ☑
 -- Judges: Spawns a random beggar, 6 room charge
+    -- Separate items for each judge?
 -- Job: Temporary Transformation? Devolves all enemies 1 level?
 -- Joel: Spawns Locusts, 3 room charge
--- Jonah: Resets angel chance to 100% on pickup, +1 speed, +.5 damage
+
+-- 1 Angel, 1 Devil, 1 neutral? 
+-- (Chapter & verse)
+-- John 11:35?
+-- Exodus: Devil Item: golden calf? (2 hearts for 4 bone hearts?)
 ----------------
 
 local game = Game()
 local mod = RegisterMod("Old Testament", 1)
 
 
--- Song of Solomon: Charms all enemies in room, 4 room charge ✅
+-- Song of Solomon (neutral): Charms all enemies in room, 4 room charge ✅
 local sosBook = Isaac.GetItemIdByName("Song of Solomon")
 function mod:SongOfSolomonUse(item)    
     local roomEntities = Isaac.GetRoomEntities()
@@ -31,7 +37,7 @@ end
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.SongOfSolomonUse, sosBook)
 
 
--- Exodus: Opens a random door in the room (Like doorstop) Maybe use -isFirstEnemyDead?
+-- Exodus (neutral): Opens a random door in the room (Like doorstop) Maybe use -isFirstEnemyDead?
 local exodusBook = Isaac.GetItemIdByName("Exodus")
 function mod:ExodusUse(item)
     local door_closed = true
@@ -55,3 +61,36 @@ function mod:ExodusUse(item)
 end
 
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.ExodusUse, exodusBook)
+
+
+-- Jonah: Resets angel chance to 100% on pickup, +.5 speed, +.5 damage
+-- https://wofsauge.github.io/IsaacDocs/rep/faq/faq.html?h=ange#how-do-i-modify-the-devil-room-angel-room-chances
+-- https://github.com/maya-bee/repentance-tutorials/tree/main
+local jonahBook = Isaac.GetItemIdByName("Jonah")
+local jonahDamage = .5
+local jonahSpeed = .5
+local firstPickup = true
+function mod:EvaluateCache(player, cacheFlags)
+    if cacheFlags & CacheFlag.CACHE_SPEED == CacheFlag.CACHE_SPEED then
+        local itemCount = player:GetCollectibleNum(jonahBook)
+        
+        -- Adds damage
+        local damageToAdd = jonahDamage * itemCount
+        player.Damage = player.Damage + damageToAdd
+        
+        -- Adds speed
+        local speedToAdd = jonahSpeed * itemCount
+        player.MoveSpeed = player.MoveSpeed + speedToAdd
+
+        -- Resets the angel room chance and forces it to be angel
+        if firstPickup == true then
+            local level = Game():GetLevel()
+            level:AddAngelRoomChance(1.0)
+            level:InitializeDevilAngelRoom(true,false)
+            level:SetStateFlag(LevelStateFlag.STATE_REDHEART_DAMAGED, false)
+            firstPickup = false
+        end
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
