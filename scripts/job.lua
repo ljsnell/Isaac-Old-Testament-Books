4c194790b6d807d88cd514be5dcd4bea3ac76b13
ljsnell <ljsnell7@gmail.com>
2024-02-16 20:23:42 -0600

    Updated with reference

--- scripts/job.lua
+++ scripts/job.lua
@@ -1,14 +1,13 @@
--- Job (neutral): After taking 8 total damage spawns two angel items
-local jobBook = Isaac.GetItemIdByName("Job")
-function MOD:JobUse(player)    
-    if player:GetCollectibleNum(jobBook) > 0 then
-        if player:GetTotalDamageTaken() > 0 then
-            --https://wofsauge.github.io/IsaacDocs/rep/Isaac.html#spawn
-            --https://www.youtube.com/watch?v=xn76bd8SccA
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE,
-                Game:GetItemPool():GetCollectible(ItemPoolType.POOL_ANGEL, false, Game():GetRoom():GetSpawnSeed()))
+-- Job (neutral): 10% Chance of spawning an item on taking damage
+function MOD:onDMG(target)
+    if target.Type == EntityType.ENTITY_PLAYER then
+        local random_num = math.random(0,10)
+        
+        if random_num == 10 then
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0,
+                Vector(320,280), Vector(0,0), nil)
         end
-    end
+  end
 end
 
-MOD:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, MOD.JobUse)
+MOD:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG,MOD.onDMG)
